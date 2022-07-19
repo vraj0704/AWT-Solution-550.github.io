976e0e42c299316cc99315be7a237124dd4f01a1
vraj0704 <vrajprajapati0704@gmail.com>
2022-07-19 15:30:47 +0530

    AWT-Solution-550

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is Javascript?",
+    answer: "Javascript is a scripting language used to make the website interactive",
+    options: [
+      "Javascript is a scripting language used to make the website interactive",
+      "Javascript is an assembly language used to make the website interactive",
+      "Javascript is a compiled language used to make the website interactive",
+      "None of the mentioned"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    answer: "It is an ordered list of values",
+    options: [
+      "It is an ordered list of objects",
+      "It is an ordered list of values",
+      "It is an ordered list of string",
+      "It is an ordered list of functions"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which of the following tag is used to embed css in html page?",
+    answer: "<style>",
+    options: [
+      "&lt;css&gt;",
+      "&lt;!DOCTYPE html&gt;",
+      "&lt;script&gt;",
+      "&lt;style&gt;"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Which of the following CSS selectors are used to specify a group of elements?",
+    answer: "class",
+    options: [
+      "tag",
+      "id",
+      "class",
+      "both class and tag"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "In which part of the HTML metadata is contained?",
+    answer: "head tag",
+    options: [
+      "head tag",
+      "title tag",
+      "html tag",
+      "body tag"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which of the following CSS property is used to make the text bold?",
+    answer: "font-weight: bold",
+    options: [
+      "text-decoration: bold",
+      "font-weight: bold",
+      "font-style: bold",
+      "text-align: bold"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Which of the following is not javascript data types?",
+    answer: "All of the mentioned",
+    options: [
+      "Null type",
+      "Undefined type",
+      "Number type",
+      "All of the mentioned"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which of the following CSS style property is used to specify an italic text?",
+    answer: "font-style",
+    options: [
+      "style",
+      "font",
+      "font-style",
+      "@font-face"
+    ]
+  },
+];
